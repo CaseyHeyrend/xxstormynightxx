aeb6e86ffe1beb0b0e23dd86ec9bc8e8612d4a8c
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2023-01-14 00:02:21 -0700

    Add files via upload

---
+++ Complete.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>XxStormyNightxX</title>
+        <link href="MainStylePage.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+    <header>
+        <img class="logo" src="images/XxStormyNightxX'slogo.png" alt=""/>
+    <nav>
+      <ul>
+         <li><a href="http://www.xxstormynightxx.com/">Home</a></li>
+        <li><a href="About.html">About</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="School.html">Projects</a></li>
+        <li><a href="Interests.html">Interests</a></li>   
+</ul>
+    </nav>
+  </header>
+        <main>
+            <h1>Thanks!</h1>
+            <h3>I'll will get back to you!.</h3>
+        </main>
+<footer>
+        <div id="Fnav">
+            <ul>
+        <li><a href="http://www.xxstormynightxx.com/">Home</a></li>
+        <li><a href="About.html">About</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="Comm310/practice/index.html">Practice</a></li>
+        <li><a href="Comm310/Portfolio_Website/styleguide.html">Style Guide</a></li>
+            </ul>
+        </div>
+        <details>
+            <summary>All content &copy; XxStormyNightxX</summary> 
+            <p>Also by Casey Heyrend</p>
+            <p>2016-2022</p>
+        </details>
+    </footer>
+    </body>
+</html>
